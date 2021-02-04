3edd229230f4e313bff1e233b4db4b611ccd0b3f
Thiago Butignon <thiagobutignon@gmail.com>
2021-02-04 17:19:53 -0300

    fix: fix imports

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,4 +1,4 @@
-import { HttpResponse, HttpRequest } from './http'
+import { HttpResponse, HttpRequest } from './'
 export interface Controller {
   handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
 }
