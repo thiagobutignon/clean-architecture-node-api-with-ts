78b8864604247b1ef5b651d3480a46fc2d3dbe78
Thiago Butignon <thiagobutignon@gmail.com>
2021-02-25 12:59:46 -0300

    feat: corrigindo bug no jest mongodb

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   mongodbMemoryServerOptions: {
-    binary: {
-      version: '4.0.3',
-      skipMD5: true
-    },
     instance: {
       dbName: 'jest'
+    },
+    binary: {
+      version: '3.6.20',
+      skipMD5: true
     },
     autoStart: false
   }
